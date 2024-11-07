1aec56d0a14633c1a114fec707f05409a0d12252
Till Herrmann <ig64xati@uni-leipzig.de>
2024-11-07 10:23:41 +0100

    initial implementation of hamlet overview

--- dataExploration/InteractiveGeoDataExplorer.py
+++ dataExploration/InteractiveGeoDataExplorer.py
@@ -10,23 +10,29 @@
 
         $ python flask_example.py
 
-    And then head to http://127.0.0.1:5000/iframe in your browser to see the map displayed
+    And then head to http://127.0.0.1:5000 in your browser to see the map displayed
 
 """
 
-from flask import Flask, render_template_string
+import bokeh
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from flask import Flask, render_template_string, render_template
 
 import folium
 import pandas as pd
 import geopandas as gpd
-
+import plotly.express as px
+from plotly import utils
+from json import dumps
+from datetime import datetime
 import hamlet_export
 
 def create_map():
     m = folium.Map(tiles="esri.WorldTopoMap", location=(14, 106), zoom_start=7)
     return m
 
-def update_map(m):
+def update_map(m, date):
     colors = {
         "00":"white",
         "SE":"green",
@@ -36,12 +42,12 @@ def update_map(m):
         "":"white",
         None:"white"
         }
-    qdate = pd.date_range("5-1-1968", periods=1, freq="MS")
+    qdate = pd.date_range(date, periods=1, freq="MS")
     folium.GeoJson(
         hamlet_export.dynamic_gdf(qdate),
         name = "Hamlet Markers",
         marker=folium.CircleMarker(radius=4, fill_color="orange", fill_opacity=0.4, color="black", weight=1),
-        tooltip=folium.GeoJsonTooltip(fields=["Hamlet Nam", "POPUL", "SCSTA", "CLASX"]),
+        popup=folium.GeoJsonPopup(fields=["Hamlet Nam", "USID", "POPUL", "SCSTA", "CLASX"]),
         style_function=lambda x: {
             "fillColor": colors[x['properties']['SCSTA']],
             "radius": (x['properties']['POPUL'])/2000+3,
@@ -57,11 +63,13 @@ def load_and_filter_shp(filename, column, value):
 app = Flask(__name__)
 
 
-@app.route("/")
-def fullscreen():
+@app.route("/date/<date>")
+def fullscreen(date):
+
     """Simple example of a fullscreen map."""
+    date = datetime.strptime(f"19{date}", "%Y%m")
     m = create_map()
-    m = update_map(m)
+    m = update_map(m, date)
     folium.LayerControl().add_to(m)
     return m.get_root().render()
 
@@ -126,6 +134,48 @@ def components():
         body_html=body_html,
         script=script,
     )
+
+@app.route("/usid/<usid>")
+def hamlet_data(usid):
+    hamladf, hesdf = hamlet_export.hamlet_history(usid)
+    print(hamladf.head())
+    name = hamladf["Hamlet Nam"].iloc[0]
+    #p = figure(x_axis_type="datetime", width=600, height=400)
+    fig1 = px.line(hamladf, x='DATE', y='CLASX')
+    fig1_json = dumps(fig1, cls=utils.PlotlyJSONEncoder)
+    fig2 = px.line(hamladf, x='DATE', y='POPUL')
+    fig2_json = dumps(fig2, cls=utils.PlotlyJSONEncoder)
+    #print(fig1_json)
+    return render_template_string(
+        """
+        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+
+
+<h3>Line Graph Representations {{name | safe}}</h3>
+
+<!-- scatter plot goes in this div -->
+<div id='fig1_div'><div>
+<div id='fig2_div'><div>
+
+<script type="text/javascript">
+
+        
+        var fig1_graph = {{fig1_json | safe}};
+        var fig2_graph = {{fig2_json | safe}};
+
+        fig1 = document.getElementById('fig1_div');
+        fig2 = document.getElementById('fig2_div');
+
+        
+        Plotly.plot(fig1, fig1_graph);
+        Plotly.plot(fig2, fig2_graph);
+
+</script>
+        """,
+        fig1_json = fig1_json,
+        fig2_json = fig2_json,
+        name = name)
 
 
 
--- dataExploration/hamlet_export.py
+++ dataExploration/hamlet_export.py
@@ -9,10 +9,15 @@ dirname = os.path.dirname(__file__)
 
 con = sqlite3.connect("hes_data.db")
 
+print("Loading Hamlet GeoData")
+
 hamlets = gpd.read_file("hamlets_shp/hamlets.shp")
+print("Loading HAMLA data")
 hamla = pd.read_sql("SELECT * from hamla", con, index_col="index", parse_dates=["DATE"])
 hamla["USID"] = pd.to_numeric(hamla["USID"])
-#hes = read_df_with_dtypes(os.path.join(dirname, "hes_data/hes_ham.csv"))
+print("Loading HES data")
+hes = pd.read_sql("SELECT * from hes_ham", con, index_col="index", parse_dates=["DATE"])
+hes["USID"] = pd.to_numeric(hes["USID_y"])
 #hes["DATE"] = pd.to_datetime(hes["DATE"])
 
 #print(hamla.dtypes)
@@ -32,3 +37,9 @@ def dynamic_gdf(daterange = None):
         df["DATE"] = df["DATE"].astype("string")
         df = gpd.GeoDataFrame(df)
     return df
+
+def hamlet_history(usid):
+    usid = int(usid)
+    hamladf = pd.merge(left=hamlets[hamlets["USID"]==usid], right=hamla, on="USID", how="left")
+    hesdf = pd.merge(left=hamlets[hamlets["USID"]==usid], right=hes, on="USID", how="left")
+    return hamladf, hesdf

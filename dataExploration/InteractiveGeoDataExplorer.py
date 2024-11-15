69f811c272efcd326e9af0f97432f3793662a409
Till Herrmann <ig64xati@uni-leipzig.de>
2024-11-15 13:20:27 +0100

    full javascript implementation of map visualisation

--- dataExploration/InteractiveGeoDataExplorer.py
+++ dataExploration/InteractiveGeoDataExplorer.py
@@ -10,11 +10,12 @@ import os
 import bokeh
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, show
-from flask import Flask, render_template_string, render_template
+from flask import Flask, make_response, render_template_string, render_template
 import geopandas as gpd
 import pandas as pd
 from plotly import utils
 import plotly.express as px
+import umsgpack
 
 import hamlet_export
 
@@ -31,59 +32,66 @@ app = Flask(__name__)
 def get_geojson(dString):
     d = datetime.strptime(dString, "%m%Y")
     qdate = pd.date_range(d, periods=1, freq="MS")
-    gdf = hamlet_export.dynamic_gdf(qdate)
-    gdf.rename(columns={"Hamlet Nam": "Hamlet Name"})
+    gdf = hamlet_export.dynamic_gdf_hamlets(qdate)
     return gdf.to_json()
+
+@app.route('/get_thor/<dString>')
+def get_thor(dString):
+    d = datetime.strptime(dString, "%m%Y")
+    qdate = pd.date_range(d, periods=1, freq="MS")
+    gdf = hamlet_export.dynamic_gdf_thor(qdate)
+    data = []
+    for i, row in gdf.iterrows():
+        data.append({"lat": row["TGTLATDD_DDD_WGS84"], "lon": row["TGTLONDDD_DDD_WGS84"], "wgt": row["WEAPONTYPEWEIGHT"]})
+    thorDict = {"max": 2000, "data": data}
+    response = make_response((umsgpack.packb(thorDict), {'content-type': 'application/octet-stream'}))
+    return response
 
 @app.route('/')
 def js_map():
-    json = get_geojson("011967")
-    return render_template("map_template.html", geojson=json)
+    hamlets = get_geojson("011967")
+    return render_template("map_template.html", hamlets=hamlets, dirname=dirname)
 
+@app.route('/heatmap.min.js')
+def heatmap():
+    f = open(os.path.join(dirname, 'templates/heatmap.js-2.0.5/build/heatmap.min.js'))
+    heatmap = f.read()
+    f.close()
+    response = make_response((heatmap, {'content-type': 'text/javascript; charset=utf-8'}))
+    return response
+
+@app.route('/leaflet-heatmap.js')
+def leaflet_heatmap():
+    f = open(os.path.join(dirname, 'templates/leaflet-heatmap.js'))
+    heatmap = f.read()
+    f.close()
+    response = make_response((heatmap, {'content-type': 'text/javascript; charset=utf-8'}))
+    return response
+
+@app.route('/msgpack.min.js')
+def msgpack():
+    f = open(os.path.join(dirname, 'templates/msgpack.js-master/msgpack.min.js'))
+    msgpack = f.read()
+    f.close()
+    response = make_response((msgpack, {'content-type': 'text/javascript; charset=utf-8'}))
+    return response
 
 @app.route("/usid/<usid>")
 def hamlet_data(usid):
     hamladf, hesdf = hamlet_export.hamlet_history(usid)
     print(hamladf.head())
-    name = hamladf["Hamlet Nam"].iloc[0]
+    name = hamladf["Hamlet Name"].iloc[0]
     #p = figure(x_axis_type="datetime", width=600, height=400)
     fig1 = px.line(hamladf, x='DATE', y='CLASX')
     fig1_json = dumps(fig1, cls=utils.PlotlyJSONEncoder)
     fig2 = px.line(hamladf, x='DATE', y='POPUL')
     fig2_json = dumps(fig2, cls=utils.PlotlyJSONEncoder)
     #print(fig1_json)
-    return render_template_string(
-        """
-        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-
-
-
-<h3>Line Graph Representations {{name | safe}}</h3>
-
-<!-- scatter plot goes in this div -->
-<div id='fig1_div'><div>
-<div id='fig2_div'><div>
-
-<script type="text/javascript">
-
-        
-        var fig1_graph = {{fig1_json | safe}};
-        var fig2_graph = {{fig2_json | safe}};
-
-        fig1 = document.getElementById('fig1_div');
-        fig2 = document.getElementById('fig2_div');
-
-        
-        Plotly.plot(fig1, fig1_graph);
-        Plotly.plot(fig2, fig2_graph);
-
-</script>
-        """,
+    return render_template(
+        "dashboard_template.html",
         fig1_json = fig1_json,
         fig2_json = fig2_json,
         name = name)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- dataExploration/hamlet_export.py
+++ dataExploration/hamlet_export.py
@@ -1,26 +1,38 @@
 """Provides the data handling methods for the flask server.
 """
+from lib2to3.pgen2 import driver
 import os
-from datetime import datetime
+from datetime import date, datetime, timedelta
 import sqlite3
 
 import geopandas as gpd
 import pandas as pd
 
 dirname = os.path.dirname(__file__)
+parentdir = "".join(dirname.split()[0:-1])
+loadpath = os.path.join(parentdir, 'HES.gpkg')
 
-con = sqlite3.connect("hes_data.db")
+con = sqlite3.connect(loadpath)
 
 print("Loading Hamlet GeoData")
 
-hamlets = gpd.read_file("hamlets_shp/hamlets.shp")
+hamlets = gpd.read_file(loadpath, layer='Hamlets')
 print("Loading HAMLA data")
-hamla = pd.read_sql("SELECT * from hamla", con, index_col="index", parse_dates=["DATE"])
-hamla["USID"] = pd.to_numeric(hamla["USID"])
+start = datetime.now()
+hamla = pd.DataFrame(gpd.read_file(loadpath, layer='HAMLA'))
+end = datetime.now()
+print(f"Time elapsed: {end-start}")
 print("Loading HES data")
-hes = pd.read_sql("SELECT * from hes_ham", con, index_col="index", parse_dates=["DATE"])
+start = datetime.now()
+hes = pd.DataFrame(gpd.read_file(loadpath, layer='HES_hamlets'))
 hes["USID"] = pd.to_numeric(hes["USID_y"])
-#hes["DATE"] = pd.to_datetime(hes["DATE"])
+end = datetime.now()
+print(f"Time elapsed: {end-start}")
+print("Loading THOR data")
+start = datetime.now()
+thor = gpd.read_file(loadpath, layer='THOR')
+end = datetime.now()
+print(f"Time elapsed: {end-start}")
 
 #print(hamla.dtypes)
 
@@ -29,7 +41,7 @@ qdate = pd.date_range("1-1-1968", periods=1, freq="MS")
 
 #df = pd.merge(right=hamla[hamla["DATE"].isin(qdate)], left=hamlets, on="USID", how="right")
 
-def dynamic_gdf(daterange = None):
+def dynamic_gdf_hamlets(daterange = None):
     if daterange is None:
         df = pd.merge(right=hamla, left=hamlets, on="USID", how="right")
         df["DATE"] = df["DATE"].astype("string")
@@ -37,11 +49,23 @@ def dynamic_gdf(daterange = None):
     elif daterange[-1] < datetime.strptime("07/1969", "%m/%Y"):
         df = pd.merge(right=hamla[hamla["DATE"].isin(daterange)], left=hamlets, on="USID", how="right")
         df["DATE"] = df["DATE"].astype("string")
+        df = df[["Hamlet Name", "USID", "POPUL", "SCSTA", "geometry"]]
         df = gpd.GeoDataFrame(df)
     else:
         df = pd.merge(right=hes[hes["DATE"].isin(daterange)], left=hamlets, on="USID", how="right")
         df["DATE"] = df["DATE"].astype("string")
+        df = df[["Hamlet Name", "USID", "HPOPUL", "HCAT", "geometry"]]
         df = gpd.GeoDataFrame(df)
+    return df
+
+def dynamic_gdf_thor(daterange = None):
+    if daterange is None:
+        df = thor
+        df = gpd.GeoDataFrame(df)
+    else:
+        df = thor[(thor["MSNDATE"] >= daterange[0]) & (thor["MSNDATE"] < (daterange[0] + timedelta(days=32)).replace(day=1))]
+        df = gpd.GeoDataFrame(df)
+    df["MSNDATE"] = df["MSNDATE"].astype("string")
     return df
 
 def hamlet_history(usid):
@@ -49,3 +73,12 @@ def hamlet_history(usid):
     hamladf = pd.merge(left=hamlets[hamlets["USID"]==usid], right=hamla, on="USID", how="left")
     hesdf = pd.merge(left=hamlets[hamlets["USID"]==usid], right=hes, on="USID", how="left")
     return hamladf, hesdf
+
+def main():
+    d = datetime.strptime("021968", "%m%Y")
+    qdate = pd.date_range(d, periods=1, freq="MS")
+    df = dynamic_gdf_thor(qdate)
+    print(df.head())
+
+if __name__ == "__main__":
+    main()
